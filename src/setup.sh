036dd33e4584d8f88f2abd22b0d64cb2ba2bae10
Kattson Bastos <kattsonbastos@gmail.com>
2023-04-16 15:48:56 +0000

    build: terraform, gcloud install

--- src/setup.sh
+++ src/setup.sh
@@ -1,21 +1,39 @@
 #!/usr/bin/env bash
 install() {
-  echo "Installing Astro CLI..."
+  echo "### Installing Astro CLI..."
  
-  curl -sSL install.astronomer.io | sudo bash -s -- v1.8.4
-  
-  ##
-  echo "Creating astro folder.."
-  #mkdir astro ## uncomment if you're running for the first time
-  cd ./astro
+  #curl -sSL install.astronomer.io | sudo bash -s -- v1.8.4
 
-  ## 
-  echo "Initializing Astro dev component.."
-  #astro dev init ## uncomment if you're running for the first time
+  # terraform
+  echo "### Installing Terraform"
+  sudo apt update && sudo apt install -y gnupg software-properties-common
 
-  ##
-  echo "Starting Airflow Services"
-  astro dev start --no-cache
+  wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+  gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+  sudo apt update
+  sudo apt install terraform
+
+  # gcloud
+  echo "### Installing GCLOUD"
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+  sudo apt update
+
+  sudo apt-get install google-cloud-sdk
+
+  gcloud init
 
 }
 
@@ -39,6 +57,6 @@ case $1 in
     up
     ;;
   *)
-    echo "Usage: $0 {up}"
+    echo "Usage: $0 {up, install}"
     ;;
 esac
